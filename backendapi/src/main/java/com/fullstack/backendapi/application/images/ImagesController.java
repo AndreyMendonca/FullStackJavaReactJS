fe3068250dde445d852c0edc9964a59cc11eec74
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-04-24 15:38:05 -0300

    Implementação do metodo save e findById

--- backendapi/src/main/java/com/fullstack/backendapi/application/images/ImagesController.java
+++ backendapi/src/main/java/com/fullstack/backendapi/application/images/ImagesController.java
@@ -5,15 +5,17 @@ import com.fullstack.backendapi.domain.enums.ImageExtension;
 import com.fullstack.backendapi.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +25,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImagemMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -31,14 +34,32 @@ public class ImagesController {
             @RequestParam("tags") List<String> tags
     ) throws IOException {
         log.info("Image recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",",tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
-        service.save(image);
-        return ResponseEntity.ok().build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image imagedSave = service.save(image);
+        URI imageUri = buildImageURL(imagedSave);
+        return ResponseEntity.created(imageUri).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String id){
+        var possibleImage = service.getById(id);
+        if(possibleImage.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        var image = possibleImage.get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        headers.setContentDispositionFormData("inline; filename=\""+image.getFileName()+"\"",image.getFileName());
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
+    }
+
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build()
+                .toUri();
     }
 }
--- backendapi/src/main/java/com/fullstack/backendapi/domain/service/ImageService.java
+++ backendapi/src/main/java/com/fullstack/backendapi/domain/service/ImageService.java
@@ -2,6 +2,9 @@ package com.fullstack.backendapi.domain.service;
 
 import com.fullstack.backendapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+    Optional<Image> getById(String id);
 }
