89212cd07e93b22c0884dcafde79bb8d6fa0f5fd
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-04-25 14:59:09 -0300

    Componente ImageCard criado

--- frontend_img/src/app/galeria/page.tsx
+++ frontend_img/src/app/galeria/page.tsx
@@ -1,9 +1,12 @@
 import { Template } from "@/components";
+import { ImageCard } from "@/components/ImageCard";
 
 const Page = () =>{
     return (
         <Template>
-            <h1>oi</h1>
+            <section className="grid grid-cols-3 gap-8">
+                <ImageCard src="https://images.squarespace-cdn.com/content/v1/5b51083c3e2d091347d2e6fe/1638462278514-4YWH839J25OURZLJUI5C/mike-benna-SBiVq9eWEtQ-unsplash.jpg" nome="Natureza" dataUpload="101010" tamanho="10"/>
+            </section>
         </Template>
     )
 }
