47cbf6d47ec8a778799ecc92213af3e0bf7cc211
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-04-25 17:07:06 -0300

    Conexão do front com o back

--- frontend_img/src/app/galeria/page.tsx
+++ frontend_img/src/app/galeria/page.tsx
@@ -1,11 +1,37 @@
+'use client'
 import { Template } from "@/components";
 import { ImageCard } from "@/components/ImageCard";
+import { Image } from "@/resources/image/image.resource";
+import { useImageService } from "@/resources/image/image.service";
+import { useState } from "react";
 
 const Page = () =>{
+
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
+    const searchImage = async () => {
+        const result = await useService.buscar();
+        setImages(result);
+        console.table(images);
+    }
+
     return (
         <Template>
+            <button onClick={searchImage} className="bg-gray-500">Buscar images</button>
             <section className="grid grid-cols-3 gap-8">
-                <ImageCard src="https://images.squarespace-cdn.com/content/v1/5b51083c3e2d091347d2e6fe/1638462278514-4YWH839J25OURZLJUI5C/mike-benna-SBiVq9eWEtQ-unsplash.jpg" nome="Natureza" dataUpload="101010" tamanho="10"/>
+                {
+                    images.map((img, index) =>(
+                       <ImageCard 
+                            key={index}
+                            src={img.url}
+                            nome={img.name}
+                            tamanho={img.size}
+                            dataUpload={img.uploadDate}
+                       />
+                    ))
+                }
             </section>
         </Template>
     )
