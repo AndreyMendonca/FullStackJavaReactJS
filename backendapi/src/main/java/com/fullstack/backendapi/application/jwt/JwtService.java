3b0b829e2b2fffe045adde6d510f63f1f648631f
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-05-05 21:09:03 -0300

    JwtService implementado junto com sua logica

---
+++ backendapi/src/main/java/com/fullstack/backendapi/application/jwt/JwtService.java
@@ -0,0 +1,49 @@
+package com.fullstack.backendapi.application.jwt;
+
+import com.fullstack.backendapi.domain.AcessToken;
+import com.fullstack.backendapi.domain.entity.User;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final SecretKeyGenerator keyGenerator;
+
+    public AcessToken generateToken(User user){
+
+        SecretKey key = keyGenerator.getKey();
+        Date expirationDate = generateExpirationDate();
+        Map<String, Object> claims = generateTokenClaims(user);
+
+        String token = Jwts
+                .builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
+        return new AcessToken(token);
+    }
+
+    private Date generateExpirationDate(){
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+        return claims;
+    }
+}
--- backendapi/src/main/java/com/fullstack/backendapi/application/users/UserServiceImpl.java
+++ backendapi/src/main/java/com/fullstack/backendapi/application/users/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fullstack.backendapi.application.users;
 
+import com.fullstack.backendapi.application.jwt.JwtService;
 import com.fullstack.backendapi.domain.AcessToken;
 import com.fullstack.backendapi.domain.entity.User;
 import com.fullstack.backendapi.domain.exception.DuplicatedTuplaException;
@@ -7,6 +8,7 @@ import com.fullstack.backendapi.domain.service.UserService;
 import com.fullstack.backendapi.infra.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,12 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
 
     @Override
     public User getByEmail(String email) {
@@ -28,11 +36,29 @@ public class UserServiceImpl implements UserService {
         if(possibleUser != null){
             throw new DuplicatedTuplaException("User already exists");
         }
+        encodePassword(user);
         return repository.save(user);
     }
 
     @Override
     public AcessToken authentication(String email, String password) {
+        var user = getByEmail(email);
+        if(user == null){
+            return null;
+        }
+        boolean matches = passwordEncoder.matches(password, user.getPassword());
+
+        if(matches){
+            return jwtService.generateToken(user);
+        }
+
         return null;
+    }
+
+    private void encodePassword(User user){
+        String rawPassword = user.getPassword();
+        String encodePassword = passwordEncoder.encode(rawPassword);
+        user.setPassword(encodePassword);
+
     }
 }
